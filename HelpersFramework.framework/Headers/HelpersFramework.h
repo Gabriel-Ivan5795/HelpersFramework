66044b24796d7275f337e67666531f19a6227c1e
Ivan Gabriel <gabriel.ivan@roweb.ro>
2021-08-05 11:00:04 +0300

    1

---
+++ HelpersFramework.framework/Headers/HelpersFramework.h
@@ -0,0 +1,18 @@
+//
+//  HelpersFramework.h
+//  HelpersFramework
+//
+//  Created by Ivan Gabriel on 04.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelpersFramework.
+FOUNDATION_EXPORT double HelpersFrameworkVersionNumber;
+
+//! Project version string for HelpersFramework.
+FOUNDATION_EXPORT const unsigned char HelpersFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelpersFramework/PublicHeader.h>
+
+
