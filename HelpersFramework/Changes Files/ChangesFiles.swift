e33ce80602774ff6f3d0eeb2d8252f7a9d8a30f2
Ivan Gabriel <gabriel.ivan@roweb.ro>
2021-08-05 10:38:02 +0300

    new changes

---
+++ HelpersFramework/Changes Files/ChangesFiles.swift
@@ -0,0 +1,17 @@
+//
+//  ChangesFiles.swift
+//  HelpersFramework
+//
+//  Created by Ivan Gabriel on 05.08.2021.
+//
+
+import UIKit
+
+class ChangesFiles: NSObject {
+
+    override init() {
+        super.init()
+        
+        print("test")
+    }
+}
